b77df559d89171ff0e1f26b3215ef36bf6900f4a
Wyatt Paquin <paquinwyatt@gmail.com>
2017-10-08 14:35:31 -0400

    Initial upload

---
+++ XMLParsing/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace XMLParsing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFindingChildren = new System.Windows.Forms.Button();
+            this.btnGetElementByTagName = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFindingChildren
+            // 
+            this.btnFindingChildren.Location = new System.Drawing.Point(109, 21);
+            this.btnFindingChildren.Name = "btnFindingChildren";
+            this.btnFindingChildren.Size = new System.Drawing.Size(100, 59);
+            this.btnFindingChildren.TabIndex = 7;
+            this.btnFindingChildren.Text = "Get Stock Names";
+            this.btnFindingChildren.UseVisualStyleBackColor = true;
+            this.btnFindingChildren.Click += new System.EventHandler(this.btnFindingChildren_Click);
+            // 
+            // btnGetElementByTagName
+            // 
+            this.btnGetElementByTagName.Location = new System.Drawing.Point(425, 21);
+            this.btnGetElementByTagName.Name = "btnGetElementByTagName";
+            this.btnGetElementByTagName.Size = new System.Drawing.Size(99, 59);
+            this.btnGetElementByTagName.TabIndex = 8;
+            this.btnGetElementByTagName.Text = "Get Revision Date";
+            this.btnGetElementByTagName.UseVisualStyleBackColor = true;
+            this.btnGetElementByTagName.Click += new System.EventHandler(this.btnGetElementByTagName_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(109, 123);
+            this.txtResults.Margin = new System.Windows.Forms.Padding(2);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ReadOnly = true;
+            this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResults.Size = new System.Drawing.Size(415, 271);
+            this.txtResults.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(244, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 59);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Get all nodes and text";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnReadXMLValues_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 423);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.btnGetElementByTagName);
+            this.Controls.Add(this.btnFindingChildren);
+            this.Name = "Form1";
+            this.Text = "Set output as a console application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnFindingChildren;
+        private System.Windows.Forms.Button btnGetElementByTagName;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ XMLParsing/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace XMLParsing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+        private void btnReadXMLFile_Click(object sender, EventArgs e)
+        {
+            // make life easy for now, put this file in the debug directory
+            StringBuilder results = new StringBuilder();
+            string sStartupPath = Application.StartupPath + "\\ContactsManager.xml";
+            if (File.Exists(sStartupPath))
+            {
+                XmlTextReader textReader = new XmlTextReader(sStartupPath);
+                textReader.Read();
+                // Check to see that you have a valid node
+                while (textReader.Read())
+                {
+                    // Move to fist element
+                    textReader.MoveToElement();
+                    Console.WriteLine("XmlTextReader Properties Test");
+                    results.AppendLine("XmlTextReader Properties Test");
+                    Console.WriteLine("===================");
+                    results.AppendLine("===================");
+                    Console.WriteLine("===================");
+                    results.AppendLine("===================");
+
+                    // Read this element's properties and display them on console
+
+                    System.Console.WriteLine("Name:" + textReader.Name);
+                    results.AppendLine("Name:" + textReader.Name);
+
+                    Console.WriteLine("Base URI:" + textReader.BaseURI);
+                    results.AppendLine("Base URI:" + textReader.BaseURI);
+
+                    Console.WriteLine("Local Name:" + textReader.LocalName);
+                    results.AppendLine("Local Name:" + textReader.LocalName);
+
+                    Console.WriteLine("Attribute Count:" + textReader.AttributeCount.ToString());
+                    results.AppendLine("Attribute Count:" + textReader.AttributeCount.ToString());
+
+                    Console.WriteLine("Depth:" + textReader.Depth.ToString());
+                    results.AppendLine("Depth:" + textReader.Depth.ToString());
+
+                    Console.WriteLine("Line Number:" + textReader.LineNumber.ToString());
+                    results.AppendLine("Line Number:" + textReader.LineNumber.ToString());
+
+                    Console.WriteLine("Node Type:" + textReader.NodeType.ToString());
+                    results.AppendLine("Node Type:" + textReader.NodeType.ToString());
+
+                    Console.WriteLine("Attribute Count:" + textReader.Value.ToString());
+                    results.AppendLine("Attribute Count:" + textReader.Value.ToString());
+
+                }
+                txtResults.Text = results.ToString();
+            }
+            else
+                Console.Write("File does not exist");
+        }
+
+        private void btnReadXMLValues_Click(object sender, EventArgs e)
+        {
+
+
+            string strFilename = Application.StartupPath + "\\ContactsManager.xml";
+            StringBuilder results = new StringBuilder();
+            XmlDocument xmlDoc = new XmlDocument();
+
+
+            xmlDoc.Load(strFilename);
+
+
+
+            XmlElement xelRoot = xmlDoc.DocumentElement;
+            XmlNodeList xnlNodes = xelRoot.SelectNodes("/ContactsManager/Contact");
+
+            foreach (XmlNode xndNode in xnlNodes)
+            {
+                results.AppendLine("First Name: " + xndNode["FirstName"].InnerText);
+                results.AppendLine("Last Name: " + xndNode["LastName"].InnerText);
+                results.AppendLine("Phone: " + xndNode["Phone"].InnerText);
+                results.AppendLine("Fax: " + xndNode["Fax"].InnerText);
+                results.AppendLine("Email: " + xndNode["Email"].InnerText);
+                results.AppendLine(" ");
+            }
+
+
+
+
+
+
+            txtResults.Text = results.ToString();
+
+        }
+
+        private void btnInnerText_Click(object sender, EventArgs e)
+        {
+            string strFilename = Application.StartupPath + "\\ContactsManager.xml";
+            XmlDocument xmlDoc = new XmlDocument();
+            StringBuilder results = new StringBuilder();
+
+            if (File.Exists(strFilename))
+            {
+                xmlDoc.Load(strFilename);
+                XmlElement elm = xmlDoc.DocumentElement;
+                Console.WriteLine("{0}", elm.InnerText);
+                results.AppendLine(elm.InnerText);
+                Console.WriteLine("{0}", elm.OuterXml);
+                results.AppendLine(elm.OuterXml);
+                Console.WriteLine("{0}", elm.InnerXml);
+                results.AppendLine(elm.InnerXml);
+            }
+            else
+                Console.WriteLine("The file {0} could not be located",
+                                  strFilename);
+            txtResults.Text = results.ToString();
+        }
+
+        private void btnFindingChildren_Click(object sender, EventArgs e)
+        {
+            string strFilename = Application.StartupPath + "\\ContactsManager.xml";
+            StringBuilder results = new StringBuilder();
+            XmlDocument xmlDoc = new XmlDocument();
+
+
+            xmlDoc.Load(strFilename);
+
+
+
+            XmlElement xelRoot = xmlDoc.DocumentElement;
+            XmlNode xnlNodeVDate = xelRoot.SelectSingleNode("/ContactsManager/VersionDate");
+            XmlNode xnlNodeVNum = xelRoot.SelectSingleNode("/ContactsManager/VersionNumber");
+
+
+            results.AppendLine("Version Date: " + xnlNodeVDate.InnerText);
+            
+            results.AppendLine(" ");
+            results.AppendLine("Version Number: " + xnlNodeVNum.InnerText);
+
+
+
+
+
+
+
+            txtResults.Text = results.ToString();
+
+        }
+
+        private void btnGetElementByTagName_Click(object sender, EventArgs e)
+        {
+            string strFilename = Application.StartupPath + "\\ContactsManager.xml";
+            StringBuilder results = new StringBuilder();
+            XmlDocument xmlDoc = new XmlDocument();
+
+
+            xmlDoc.Load(strFilename);
+            XmlElement elm = xmlDoc.DocumentElement;
+            XmlNodeList stocks = xmlDoc.GetElementsByTagName("RevisionDate");
+
+            // Now you can check each node of the list
+            foreach (XmlNode node in stocks)
+            {
+                Console.WriteLine(node.InnerText);
+                results.AppendLine(node.InnerText);
+            }
+            txtResults.Text = results.ToString();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
